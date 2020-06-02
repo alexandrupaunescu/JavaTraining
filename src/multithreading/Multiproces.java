af137d3ff97eec78090511f6a5f5c4396c527be3
GAP <GAP@GAP-WIN10>
2020-06-02 14:12:24 +0300

    Solved multithreading

--- src/multithreading/Multiproces.java
+++ src/multithreading/Multiproces.java
@@ -26,21 +26,13 @@ public class Multiproces {
         try {
             ProcessBuilder pb;
 // Use process builder to start 2 processes multithreading.SayHello with parameter
-            pb = new ProcessBuilder(
-                    "java",
-                    "-classpath",
-                    "C:/Users/Dinamo/Desktop/Javaprocess",
-                    "multithreading.SayHello",
-                    "2", "John", "Jane");
-            Map<String, String> env = pb.environment();
-            env.put("TEXT_CONGRATS", "Congratulations for starting your process! ");
-            env.remove("OTHERVAR");
-            env.put("TEXT_HI", "Hello ");
-            pb.directory(new File("/Users/Dinamo/Documents/NetBeansProjectsAStiRo/javatraninggastiro/JavaTraining/src/multithreading", "multithreading.Multiproces"));
-
+            pb = new ProcessBuilder("Notepad.exe");
+            
             Process p1 = pb.start();
 
             Process p2 = pb.start();
+            
+            Process p3 = pb.start();
 
         } catch (IOException ex) {
             Logger.getLogger(Multiprocess.class.getName()).log(Level.SEVERE, "An IO exception is here!", ex);
